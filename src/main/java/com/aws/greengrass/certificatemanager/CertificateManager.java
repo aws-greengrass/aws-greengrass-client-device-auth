331e9cda14c6740667f7e8d21c00ef372c3ad9d0
Joe Butler <508579+jbutler@users.noreply.github.com>
2021-04-12 17:28:38 -0600

    chore: remove redundant certificate downloader (#11)

--- src/main/java/com/aws/greengrass/certificatemanager/CertificateManager.java
+++ src/main/java/com/aws/greengrass/certificatemanager/CertificateManager.java
@@ -5,11 +5,9 @@
 
 package com.aws.greengrass.certificatemanager;
 
-import com.aws.greengrass.certificatemanager.certificate.CertificateDownloader;
 import com.aws.greengrass.certificatemanager.certificate.CertificateHelper;
 import com.aws.greengrass.certificatemanager.certificate.CertificateStore;
 import com.aws.greengrass.certificatemanager.certificate.CsrProcessingException;
-import com.aws.greengrass.certificatemanager.model.DeviceConfig;
 import com.aws.greengrass.logging.api.Logger;
 import com.aws.greengrass.logging.impl.LogManager;
 import lombok.NonNull;
@@ -27,33 +25,23 @@ import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Consumer;
-import java.util.stream.Collectors;
 import javax.inject.Inject;
 
 public class CertificateManager {
     private static final long DEFAULT_CERT_EXPIRY_SECONDS = 60 * 60 * 24 * 7; // 1 week
-    private static final String DEVICE_ARN = "deviceArn";
-    private static final String CERTIFICATE_ID = "certificateId";
 
-    private final CertificateDownloader certificateDownloader;
     private final Logger logger = LogManager.getLogger(CertificateManager.class);
-    private final Map<DeviceConfig, String> deviceCertificateMap = new ConcurrentHashMap<>();
 
     private final CertificateStore certificateStore;
 
     /**
      * Constructor.
      *
-     * @param certificateDownloader IoT device certificate downloader
      * @param certificateStore      Helper class for managing certificate authorities
      */
     @Inject
-    public CertificateManager(CertificateDownloader certificateDownloader, CertificateStore certificateStore) {
-        this.certificateDownloader = certificateDownloader;
+    public CertificateManager(CertificateStore certificateStore) {
         this.certificateStore = certificateStore;
     }
 
@@ -65,66 +53,6 @@ public class CertificateManager {
      */
     public void update(String caPassphrase, CertificateStore.CAType caType) throws KeyStoreException {
         certificateStore.update(caPassphrase, caType);
-    }
-
-    /**
-     * Set device configurations.
-     *
-     * @param deviceConfigurationList Updated device configuration list
-     */
-    void setDeviceConfigurations(List<DeviceConfig> deviceConfigurationList) {
-        Set<DeviceConfig> currentDeviceSet = deviceCertificateMap.keySet();
-        Set<DeviceConfig> newDeviceSet = deviceConfigurationList.stream().collect(Collectors.toSet());
-
-        currentDeviceSet.forEach(d -> {
-            if (!newDeviceSet.contains(d)) {
-                logger.atInfo()
-                        .kv(DEVICE_ARN, d.getDeviceArn())
-                        .kv(CERTIFICATE_ID, d.getCertificateId())
-                        .log("removing untrusted device");
-                deviceCertificateMap.remove(d);
-            }
-        });
-
-        // TODO: handle exceptions
-        newDeviceSet.forEach(d -> {
-            deviceCertificateMap.computeIfAbsent(d, k -> {
-                logger.atInfo()
-                        .kv(DEVICE_ARN, d.getDeviceArn())
-                        .kv(CERTIFICATE_ID, d.getCertificateId())
-                        .log("adding trusted device");
-                String certPem = loadDeviceCertificate(d.getCertificateId());
-                try {
-                    certificateStore.storeDeviceCertificateIfNotPresent(d.getCertificateId(), certPem);
-                } catch (IOException e) {
-                    logger.atError()
-                            .cause(e)
-                            .log("unable to store device certificate");
-                }
-                return certPem;
-            });
-        });
-    }
-
-    private String loadDeviceCertificate(String certificateId) {
-        try {
-            return certificateStore.loadDeviceCertificate(certificateId);
-        } catch (IOException e) {
-            logger.atInfo()
-                    .kv(CERTIFICATE_ID, certificateId)
-                    .log("certificate not found in local store, attempting to download it");
-            return certificateDownloader.downloadSingleDeviceCertificate(certificateId);
-        }
-    }
-
-    /**
-     * Return a map of device certificates, indexed by Device ARN.
-     */
-    Map<String, String> getDeviceCertificates() {
-        return deviceCertificateMap.entrySet().stream().collect(Collectors.toMap(
-                entry -> entry.getKey().getDeviceArn(),
-                entry -> entry.getValue()
-        ));
     }
 
     /**
@@ -167,47 +95,6 @@ public class CertificateManager {
             PKCS10CertificationRequest pkcs10CertificationRequest =
                     CertificateHelper.getPKCS10CertificationRequestFromPem(csr);
             X509Certificate certificate = CertificateHelper.signServerCertificateRequest(
-                    certificateStore.getCACertificate(),
-                    certificateStore.getCAPrivateKey(),
-                    pkcs10CertificationRequest,
-                    Date.from(now),
-                    Date.from(now.plusSeconds(DEFAULT_CERT_EXPIRY_SECONDS)));
-
-            // TODO: Save cb
-            // For now, just generate certificate and accept it
-            cb.accept(certificate);
-        } catch (KeyStoreException e) {
-            logger.atError().setCause(e).log("unable to subscribe to certificate update");
-            throw e;
-        } catch (RuntimeException | OperatorCreationException | NoSuchAlgorithmException | CertificateException
-                | InvalidKeyException | IOException e) {
-            throw new CsrProcessingException(csr, e);
-        }
-    }
-
-    /**
-     * Subscribe to client certificate updates.
-     * <p>
-     * The certificate manager will save the given CSR and generate a new certificate under the following scenarios:
-     *   1) The previous certificate is nearing expiry
-     * Certificates will continue to be generated until the client calls unsubscribeFromCertificateUpdates.
-     * </p>
-     *
-     * @param csr Certificate signing request
-     * @param cb  Certificate consumer
-     * @throws KeyStoreException if unable to access KeyStore
-     * @throws CsrProcessingException if unable to process CSR
-     */
-    @SuppressWarnings("PMD.AvoidCatchingGenericException")
-    public void subscribeToClientCertificateUpdates(@NonNull String csr, @NonNull Consumer<X509Certificate> cb)
-            throws KeyStoreException, CsrProcessingException {
-        // BouncyCastle can throw RuntimeExceptions, and unfortunately it is not easy to detect
-        // bad input beforehand. For now, just catch and re-throw a CsrProcessingException
-        try {
-            Instant now = Instant.now();
-            PKCS10CertificationRequest pkcs10CertificationRequest =
-                    CertificateHelper.getPKCS10CertificationRequestFromPem(csr);
-            X509Certificate certificate = CertificateHelper.signClientCertificateRequest(
                     certificateStore.getCACertificate(),
                     certificateStore.getCAPrivateKey(),
                     pkcs10CertificationRequest,
