b793df52ae2bddc906415a25516e99fafa1a2a49
Jason <zhengang@amazon.com>
2021-05-05 13:01:19 -0700

    fix: remove non-null check of AutherizationRequest::clientId

--- src/main/java/com/aws/greengrass/device/AuthorizationRequest.java
+++ src/main/java/com/aws/greengrass/device/AuthorizationRequest.java
@@ -10,5 +10,5 @@ public class AuthorizationRequest {
     @NonNull String operation;
     @NonNull String resource;
     @NonNull String sessionId;
-    @NonNull String clientId;
+    String clientId;
 }
