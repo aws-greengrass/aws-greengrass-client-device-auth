bec36c94c6cf36b283a7845568ce451f0429a28a
Anmol Popli <popanmol@amazon.com>
2021-05-05 14:29:44 -0700

    fix: isValidIP and tests

--- src/main/java/com/aws/greengrass/util/ParseIPAddress.java
+++ src/main/java/com/aws/greengrass/util/ParseIPAddress.java
@@ -32,17 +32,66 @@ public final class ParseIPAddress {
         //  IPV4
         Matcher ipv4Matcher = IPV4_PAT.matcher(host);
         if (ipv4Matcher.matches()) {
+            for (int i = 1; i <= 4; i++) {
+                if (!isValidHex4(ipv4Matcher.group(i))) {
+                    return false;
+                }
+            }
             return true;
         }
 
         //  IPV6, double colon
         Matcher ipv6DoubleColonMatcher = IPV6_DOUBL_COL_PAT.matcher(host);
         if (ipv6DoubleColonMatcher.matches()) {
-            return true;
+            String p1 = ipv6DoubleColonMatcher.group(1);
+            if (p1.isEmpty()) {
+                p1 = "0";
+            }
+            String p2 = ipv6DoubleColonMatcher.group(2);
+            if (p2.isEmpty()) {
+                p2 = "0";
+            }
+            host =  p1 + getZero(8 - numCount(p1) - numCount(p2)) + p2;
+            if (ipv6DoubleColonMatcher.group(3) != null) {
+                host = "[" + host + "]:" + ipv6DoubleColonMatcher.group(3);
+            }
         }
 
         //  IPV6
         Matcher ipv6Matcher = IPV6_PAT.matcher(host);
-        return ipv6Matcher.matches();
+        if (ipv6Matcher.matches()) {
+            for (int i = 1; i <= 8; i++) {
+                if (!isValidHex6(ipv6Matcher.group(i))) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        return false;
+    }
+
+    private static int numCount(String s) {
+        return s.split(":").length;
+    }
+
+    private static String getZero(int count) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(':');
+        while (count > 0) {
+            sb.append("0:");
+            count--;
+        }
+        return sb.toString();
+    }
+
+    private static boolean isValidHex4(String s) {
+        int val = Integer.parseInt(s);
+        return val >= 0 && val <= 255;
+    }
+
+    private static boolean isValidHex6(String s) {
+        int val = Integer.parseInt(s, 16);
+        return val >= 0 && val <= 65_536;
     }
 }
