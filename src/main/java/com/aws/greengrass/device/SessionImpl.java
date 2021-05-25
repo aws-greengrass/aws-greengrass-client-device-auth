992ed26c0814b92d131dfd44a8275b08053d12c0
Jason <zhengang@amazon.com>
2021-05-25 09:43:18 -0700

    chore: add copyright

--- src/main/java/com/aws/greengrass/device/SessionImpl.java
+++ src/main/java/com/aws/greengrass/device/SessionImpl.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package com.aws.greengrass.device;
 
 import com.aws.greengrass.device.attribute.AttributeProvider;
