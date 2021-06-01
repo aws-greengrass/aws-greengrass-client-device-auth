5225ca6ec5d62179c59cb922ccae6711b23024f5
Jason <zhengang@amazon.com>
2021-06-01 11:45:38 -0700

    chore: remove session healthy check

--- src/main/java/com/aws/greengrass/device/SessionManager.java
+++ src/main/java/com/aws/greengrass/device/SessionManager.java
@@ -5,76 +5,26 @@
 
 package com.aws.greengrass.device;
 
-import com.aws.greengrass.certificatemanager.certificate.CertificateStore;
-import com.aws.greengrass.device.attribute.AttributeProvider;
-import com.aws.greengrass.device.attribute.DeviceAttribute;
 import com.aws.greengrass.device.exception.AuthorizationException;
-import com.aws.greengrass.device.exception.CloudServiceInteractionException;
 import com.aws.greengrass.device.iot.Certificate;
-import com.aws.greengrass.device.iot.IotAuthClient;
-import com.aws.greengrass.device.iot.Thing;
 import com.aws.greengrass.logging.api.Logger;
 import com.aws.greengrass.logging.impl.LogManager;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
-import java.io.IOException;
-import java.time.Instant;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.TimeUnit;
-import java.util.function.Function;
-import javax.inject.Inject;
 
 /**
  * Singleton class for managing AuthN and AuthZ session.
  */
 public class SessionManager {
     private static final Logger logger = LogManager.getLogger(SessionManager.class);
-    private static final long SESSION_CHECK_DELAY_IN_DAYS = 7L;
-    private static final TimeUnit SESSION_CHECK_TIME_UNIT = TimeUnit.DAYS;
-    private static final long SESSION_EXPIRY_CHECK_DELAY_IN_MINUTES = 20L;
-    private static final TimeUnit SESSION_EXPIRY_CHECK_TIME_UNIT = TimeUnit.MINUTES;
-    private static final long ONE_HOUR_IN_MILLISECONDS = 60 * 60 * 1000L;
     public static final String SESSION_ID = "sessionId";
-
-    private final IotAuthClient iotAuthClient;
-    private final CertificateStore certificateStore;
-    private final ScheduledExecutorService scheduledExecutorService;
 
     @Setter(AccessLevel.PACKAGE)
     private Map<String, Session> sessionMap = new ConcurrentHashMap<>();
-
-    @Setter(AccessLevel.PACKAGE)
-    private ScheduledFuture<?> sessionRefreshFuture;
-
-    @Setter(AccessLevel.PACKAGE)
-    private ScheduledFuture<?> sessionExpiryCheckFuture;
-
-    /**
-     * Session Manager constructor.
-     *
-     * @param iotAuthClient            cloud service client
-     * @param certificateStore         certificate store
-     * @param scheduledExecutorService scheduling service
-     */
-    @Inject
-    public SessionManager(IotAuthClient iotAuthClient, CertificateStore certificateStore,
-                          ScheduledExecutorService scheduledExecutorService) {
-        this.iotAuthClient = iotAuthClient;
-        this.certificateStore = certificateStore;
-        this.scheduledExecutorService = scheduledExecutorService;
-    }
 
     /**
      * find session by id.
@@ -83,12 +33,7 @@ public class SessionManager {
      * @return session or null
      */
     public Session findSession(String sessionId) {
-        return sessionMap.computeIfPresent(sessionId, (k, v) -> {
-            // extend session expiry of cache hit
-            SessionDecorator session = (SessionDecorator) v;
-            session.setLastVisit(Instant.now());
-            return session;
-        });
+        return sessionMap.getOrDefault(sessionId, null);
     }
 
     /**
@@ -99,7 +44,7 @@ public class SessionManager {
      */
     public String createSession(Certificate certificate) {
         String sessionId = generateSessionId();
-        sessionMap.put(sessionId, new SessionDecorator(sessionId, new SessionImpl(certificate), Instant.now()));
+        sessionMap.put(sessionId, new SessionImpl(certificate));
         logger.atInfo().kv(SESSION_ID, sessionId).log("Created the session");
         return sessionId;
     }
@@ -120,175 +65,6 @@ public class SessionManager {
         Session session = sessionMap.get(sessionId);
         if (!sessionMap.remove(sessionId, session)) {
             throw new AuthorizationException(String.format("No session is associated with session id (%s)", sessionId));
-        }
-    }
-
-    private void closeSession(Session session, String reason) {
-        SessionDecorator sessionDecorator = (SessionDecorator) session;
-        logger.atInfo().kv("session", sessionDecorator.getSession())
-                .log(String.format("Close the session because %s", reason));
-        try {
-            closeSession(sessionDecorator.getSessionId());
-        } catch (AuthorizationException e) {
-            logger.atInfo().cause(e).kv(SESSION_ID, sessionDecorator.getSessionId()).log(e.getMessage());
-        }
-    }
-
-    /**
-     * Start session healthy check.
-     */
-    public void startSessionCheck() {
-        sessionRefreshFuture = scheduledExecutorService
-                .scheduleWithFixedDelay(this::refreshSessions, 0, SESSION_CHECK_DELAY_IN_DAYS, SESSION_CHECK_TIME_UNIT);
-    }
-
-    /**
-     * Stop session healthy check.
-     */
-    public void stopSessionCheck() {
-        if (sessionRefreshFuture != null) {
-            sessionRefreshFuture.cancel(true);
-        }
-    }
-
-    /**
-     * refresh the session, close the session if certificate or thing is invalid.
-     */
-    void refreshSessions() {
-        Map<String, List<Session>> certificateToSessionsMap = getCertificateToSessionsMap();
-        for (Map.Entry<String, List<Session>> entry : certificateToSessionsMap.entrySet()) {
-            if (shouldCloseSessionsForCertificate(entry.getKey())) {
-                // close all sessions with this certificate
-                for (Session session : entry.getValue()) {
-                    closeSession(session, "certificate is not valid");
-                }
-            } else {
-                for (Session session : entry.getValue()) {
-                    if (shouldCloseSessionForThing(session)) {
-                        // thing no other attribute matching supported than thingName, close the session
-                        // TODO detach the thing from the session
-                        closeSession(session, "thing is not associated with certificate");
-                    }
-                }
-            }
-        }
-    }
-
-    private Map<String, List<Session>> getCertificateToSessionsMap() {
-        Map<String, List<Session>> certificateToSessionsMap = new HashMap<>();
-        for (Map.Entry<String, Session> entry : sessionMap.entrySet()) {
-            Certificate certificate = (Certificate) entry.getValue().getAttributeProvider(Certificate.NAMESPACE);
-            List<Session> sessions = certificateToSessionsMap
-                    .computeIfAbsent(certificate.getCertificateHash(), (k) -> new ArrayList<>());
-            sessions.add(entry.getValue());
-        }
-        return certificateToSessionsMap;
-    }
-
-    private boolean shouldCloseSessionsForCertificate(String certificatePemHash) {
-        String certificatePem;
-        try {
-            certificatePem = certificateStore.loadDeviceCertificate(certificatePemHash);
-        } catch (IOException e) {
-            logger.atError().cause(e).kv("certificatePemHash", certificatePemHash)
-                    .log("Failed to load certificate saved on disk");
-            return true;
-        }
-
-        if (!certificatePemHash.equals(CertificateStore.computeCertificatePemHash(certificatePem))) {
-            logger.atError().kv("certificatePemHash", certificatePemHash).log("certificate got modified.");
-            return true;
-        }
-
-        try {
-            Optional<String> certificateId = iotAuthClient.getActiveCertificateId(certificatePem);
-            return !certificateId.isPresent();
-        } catch (CloudServiceInteractionException e) {
-            logger.atError().cause(e).kv("certificatePem", certificatePem).log("Can't verify client certificate");
-        }
-        return false;
-    }
-
-    private boolean shouldCloseSessionForThing(Session session) {
-        Thing thing = (Thing) session.getAttributeProvider(Thing.NAMESPACE);
-        if (thing != null) {
-            Certificate certificate = (Certificate) session.getAttributeProvider(Certificate.NAMESPACE);
-            try {
-                return !iotAuthClient.isThingAttachedToCertificate(thing, certificate);
-            } catch (CloudServiceInteractionException e) {
-                // In case of cloud service availability issue, log the error but assume valid till next check
-                logger.atError().cause(e).kv("thingName", thing.getThingName())
-                        .kv("certificateId", certificate.getIotCertificateId())
-                        .log("Can't verify thing certificate association");
-            }
-        }
-        return false;
-    }
-
-    /**
-     * Start session expiry check.
-     */
-    public void startSessionExpiryCheck() {
-        sessionExpiryCheckFuture = scheduledExecutorService
-                .scheduleWithFixedDelay(this::checkSessionExpiry, 0, SESSION_EXPIRY_CHECK_DELAY_IN_MINUTES,
-                        SESSION_EXPIRY_CHECK_TIME_UNIT);
-    }
-
-    /**
-     * Stop session expiry check.
-     */
-    public void stopSessionExpiryCheck() {
-        if (sessionExpiryCheckFuture != null) {
-            sessionExpiryCheckFuture.cancel(true);
-        }
-    }
-
-    void checkSessionExpiry() {
-        Instant now = Instant.now();
-        sessionMap.entrySet().removeIf(entry -> {
-            SessionDecorator session = (SessionDecorator) entry.getValue();
-            if (session.getLastVisit() != null && session.getLastVisit().plusMillis(ONE_HOUR_IN_MILLISECONDS)
-                    .isBefore(now)) {
-                logger.atDebug().kv(SESSION_ID, session.getSessionId())
-                        .kv("lastVisit", session.getLastVisit())
-                        .kv("maxInactiveInterval", ONE_HOUR_IN_MILLISECONDS)
-                        .log("Session exceeds max inactive interval, close the session.");
-                return true;
-            }
-            return false;
-        });
-    }
-
-    @AllArgsConstructor
-    @RequiredArgsConstructor
-    @Getter
-    static class SessionDecorator implements Session {
-        private final String sessionId;
-        private final Session session;
-        @Setter
-        private Instant lastVisit;
-
-        @Override
-        public AttributeProvider getAttributeProvider(String attributeProviderNameSpace) {
-            return session.getAttributeProvider(attributeProviderNameSpace);
-        }
-
-        @Override
-        public AttributeProvider putAttributeProvider(String attributeProviderNameSpace,
-                                                      AttributeProvider attributeProvider) {
-            return session.putAttributeProvider(attributeProviderNameSpace, attributeProvider);
-        }
-
-        @Override
-        public AttributeProvider computeAttributeProviderIfAbsent(String key,
-                                                                  Function<? super String, ? extends AttributeProvider>
-                                                                          mappingFunction) {
-            return session.computeAttributeProviderIfAbsent(key, mappingFunction);
-        }
-
-        @Override
-        public DeviceAttribute getSessionAttribute(String attributeNamespace, String attributeName) {
-            return session.getSessionAttribute(attributeNamespace, attributeName);
         }
     }
 }
--- src/test/java/com/aws/greengrass/device/SessionManagerTest.java
+++ src/test/java/com/aws/greengrass/device/SessionManagerTest.java
@@ -6,70 +6,22 @@
 package com.aws.greengrass.device;
 
 
-import com.aws.greengrass.certificatemanager.certificate.CertificateStore;
 import com.aws.greengrass.device.exception.AuthorizationException;
-import com.aws.greengrass.device.exception.CloudServiceInteractionException;
 import com.aws.greengrass.device.iot.Certificate;
-import com.aws.greengrass.device.iot.IotAuthClient;
-import com.aws.greengrass.device.iot.Thing;
 import com.aws.greengrass.testcommons.testutilities.GGExtension;
-import org.hamcrest.collection.IsMapContaining;
-import org.hamcrest.collection.IsMapWithSize;
-import org.hamcrest.core.Is;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.api.extension.ExtensionContext;
-import org.mockito.ArgumentCaptor;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.io.IOException;
-import java.time.Instant;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.TimeUnit;
-
-import static com.aws.greengrass.testcommons.testutilities.ExceptionLogProtector.ignoreExceptionOfType;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.nullValue;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 @ExtendWith({MockitoExtension.class, GGExtension.class})
 class SessionManagerTest {
 
-    @InjectMocks
-    private SessionManager sessionManager;
-
-    @Mock
-    private IotAuthClient iotAuthClient;
-
-    @Mock
-    private CertificateStore certificateStore;
-
-    @Mock
-    private ScheduledExecutorService scheduledExecutorService;
-
-    @Test
-    void GIVEN_createdSession_WHEN_findSession_THEN_sessionLastVisitUpdated() throws Exception {
-        String id = sessionManager.createSession(new Certificate("pem", "certificateId"));
-        SessionManager.SessionDecorator session = (SessionManager.SessionDecorator) sessionManager.findSession(id);
-        Instant firstVisit = session.getLastVisit();
-        Thread.sleep(100);
-        session = (SessionManager.SessionDecorator) sessionManager.findSession(id);
-        Instant secondVisit = session.getLastVisit();
-
-        assertThat(secondVisit.isAfter(firstVisit), Is.is(true));
-    }
+    private final SessionManager sessionManager = new SessionManager();
 
     @Test
     void GIVEN_session_exist_WHEN_close_session_THEN_succeed() throws Exception {
@@ -82,167 +34,5 @@ class SessionManagerTest {
     @Test
     void GIVEN_session_not_exist_WHEN_close_session_THEN_throw_exception() {
         assertThrows(AuthorizationException.class, () -> sessionManager.closeSession("id"));
-    }
-
-    @Test
-    void GIVEN_sessionData_WHEN_refreshSession_THEN_invalidSessionClosed() throws Exception {
-        Map<String, Session> sessionMap = prepareSessionHealthCheckData();
-        sessionManager.setSessionMap(sessionMap);
-        String pem1 = "pem1";
-        String pem2 = "pem2";
-        String pem1Hash = CertificateStore.computeCertificatePemHash(pem1);
-        String pem2Hash = CertificateStore.computeCertificatePemHash(pem2);
-        when(certificateStore.loadDeviceCertificate(pem1Hash)).thenReturn(pem1);
-        when(certificateStore.loadDeviceCertificate(pem2Hash)).thenReturn(pem2);
-        when(iotAuthClient.getActiveCertificateId(pem1)).thenReturn(Optional.empty());
-        when(iotAuthClient.getActiveCertificateId(pem2)).thenReturn(Optional.of("id2"));
-        Certificate certificate = new Certificate(pem2Hash, "id2");
-        when(iotAuthClient.isThingAttachedToCertificate(new Thing("thing2"), certificate)).thenReturn(true);
-        when(iotAuthClient.isThingAttachedToCertificate(new Thing("thing4"), certificate)).thenReturn(false);
-
-        sessionManager.startSessionCheck();
-
-        ArgumentCaptor<Runnable> runnableArgumentCaptor = ArgumentCaptor.forClass(Runnable.class);
-
-        verify(scheduledExecutorService)
-                .scheduleWithFixedDelay(runnableArgumentCaptor.capture(), eq(0L), eq(7L), eq(TimeUnit.DAYS));
-        runnableArgumentCaptor.getValue().run();
-
-        assertThat(sessionMap, IsMapWithSize.aMapWithSize(1));
-        assertThat(sessionMap, IsMapContaining.hasKey("2"));
-    }
-
-    @Test
-    void GIVEN_loadCertificateError_WHEN_refreshSession_THEN_closeSession(ExtensionContext context) throws Exception {
-        ignoreExceptionOfType(context, IOException.class);
-        Map<String, Session> sessionMap = new HashMap<>();
-        addToSessionMap("1", "pem1", "id1", "thing1", sessionMap);
-        sessionManager.setSessionMap(sessionMap);
-        when(certificateStore.loadDeviceCertificate(CertificateStore.computeCertificatePemHash("pem1")))
-                .thenThrow(IOException.class);
-
-        sessionManager.refreshSessions();
-
-        assertThat(sessionMap, IsMapWithSize.aMapWithSize(0));
-    }
-
-    @Test
-    void GIVEN_certificateModified_WHEN_refreshSession_THEN_closeSession() throws Exception {
-        Map<String, Session> sessionMap = new HashMap<>();
-        addToSessionMap("1", "pem1", "id1", "thing1", sessionMap);
-        sessionManager.setSessionMap(sessionMap);
-        when(certificateStore.loadDeviceCertificate(CertificateStore.computeCertificatePemHash("pem1")))
-                .thenReturn("pem11");
-
-        sessionManager.refreshSessions();
-
-        assertThat(sessionMap, IsMapWithSize.aMapWithSize(0));
-    }
-
-    @Test
-    void GIVEN_getActiveCertificateIdException_WHEN_refreshSession_THEN_keepSessionOpen(ExtensionContext context)
-            throws Exception {
-        ignoreExceptionOfType(context, CloudServiceInteractionException.class);
-        Map<String, Session> sessionMap = new HashMap<>();
-        String pem1 = "pem1";
-        String pem1Hash = CertificateStore.computeCertificatePemHash(pem1);
-        addToSessionMap("1", pem1, "id1", "thing1", sessionMap);
-        sessionManager.setSessionMap(sessionMap);
-        when(certificateStore.loadDeviceCertificate(pem1Hash)).thenReturn(pem1);
-        when(iotAuthClient.getActiveCertificateId(pem1)).thenThrow(CloudServiceInteractionException.class);
-        Certificate certificate = new Certificate(pem1Hash, "id1");
-        when(iotAuthClient.isThingAttachedToCertificate(new Thing("thing1"), certificate)).thenReturn(true);
-
-        sessionManager.refreshSessions();
-
-        assertThat(sessionMap, IsMapWithSize.aMapWithSize(1));
-    }
-
-    @Test
-    void GIVEN_verifyThingAssociationException_WHEN_refreshSession_THEN_keepSessionOpen(ExtensionContext context)
-            throws Exception {
-        ignoreExceptionOfType(context, CloudServiceInteractionException.class);
-        Map<String, Session> sessionMap = new HashMap<>();
-        String pem1 = "pem1";
-        String pem1Hash = CertificateStore.computeCertificatePemHash(pem1);
-        addToSessionMap("1", pem1, "id1", "thing1", sessionMap);
-        sessionManager.setSessionMap(sessionMap);
-        when(certificateStore.loadDeviceCertificate(pem1Hash)).thenReturn(pem1);
-        when(iotAuthClient.getActiveCertificateId(pem1)).thenReturn(Optional.of("id1"));
-        Certificate certificate = new Certificate(pem1Hash, "id1");
-        when(iotAuthClient.isThingAttachedToCertificate(new Thing("thing1"), certificate))
-                .thenThrow(CloudServiceInteractionException.class);
-
-        sessionManager.refreshSessions();
-
-        assertThat(sessionMap, IsMapWithSize.aMapWithSize(1));
-    }
-
-    private Map<String, Session> prepareSessionHealthCheckData() {
-        Map<String, Session> sessionMap = new HashMap<>();
-        addToSessionMap("1", "pem1", "id1", "thing1", sessionMap);
-        addToSessionMap("2", "pem2", "id2", "thing2", sessionMap);
-        addToSessionMap("3", "pem1", "id1", "thing3", sessionMap);
-        addToSessionMap("4", "pem2", "id2", "thing4", sessionMap);
-        return sessionMap;
-    }
-
-    private void addToSessionMap(String id, String certificate, String certificateId, String thingName,
-                                 Map<String, Session> sessionMap) {
-        addToSessionMap(id, certificate, certificateId, thingName, null, sessionMap);
-    }
-
-    private void addToSessionMap(String id, String certificate, String certificateId, String thingName,
-                                 Instant lastVisit, Map<String, Session> sessionMap) {
-        String certificateHash = CertificateStore.computeCertificatePemHash(certificate);
-        Session session = new SessionImpl(new Certificate(certificateHash, certificateId));
-        session.putAttributeProvider(Thing.NAMESPACE, new Thing(thingName));
-        sessionMap.put(id, new SessionManager.SessionDecorator(id, session, lastVisit));
-    }
-
-    @Test
-    void GIVEN_scheduledSessionCheckFuture_WHEN_stopSessionCheck_THEN_cancelFuture() {
-        ScheduledFuture<?> future = mock(ScheduledFuture.class);
-        sessionManager.setSessionRefreshFuture(future);
-
-        sessionManager.stopSessionCheck();
-
-        verify(future).cancel(true);
-    }
-
-    @Test
-    void GIVEN_sessionExpired_WHEN_checkSessionExpiry_THEN_closeSession() {
-        long twoHoursInMillis = 2 * 60 * 60 * 1000L;
-        Map<String, Session> sessionMap = new HashMap<>();
-        addToSessionMap("1", "pem1", "id1", "thing1", Instant.now().minusMillis(twoHoursInMillis), sessionMap);
-        addToSessionMap("2", "pem2", "id2", "thing2", Instant.now(), sessionMap);
-        sessionManager.setSessionMap(sessionMap);
-
-        sessionManager.startSessionExpiryCheck();
-
-        ArgumentCaptor<Runnable> runnableArgumentCaptor = ArgumentCaptor.forClass(Runnable.class);
-        verify(scheduledExecutorService).scheduleWithFixedDelay(runnableArgumentCaptor.capture(), eq(0L), eq(20L),
-                eq(TimeUnit.MINUTES));
-        runnableArgumentCaptor.getValue().run();
-
-        assertThat(sessionMap, IsMapWithSize.aMapWithSize(1));
-        assertThat(sessionMap, IsMapContaining.hasKey("2"));
-    }
-
-    @Test
-    void GIVEN_mockedSchedulingService_WHEN_startSessionExpiryCheck_THEN_invokeProperMethod() {
-        sessionManager.startSessionExpiryCheck();
-
-        verify(scheduledExecutorService).scheduleWithFixedDelay(any(), eq(0L), eq(20L), eq(TimeUnit.MINUTES));
-    }
-
-    @Test
-    void GIVEN_scheduledSessionCheckFuture_WHEN_stopSessionExpiryCheck_THEN_cancelFuture() {
-        ScheduledFuture<?> future = mock(ScheduledFuture.class);
-        sessionManager.setSessionExpiryCheckFuture(future);
-
-        sessionManager.stopSessionExpiryCheck();
-
-        verify(future).cancel(true);
     }
 }
