33524df1f325e9be011d5f97b313a8e491a65ae0
Saranya <nukai@amazon.com>
2022-05-20 08:30:21 -0700

    fix: check thing association with cert id during session creation (#82)

--- src/test/java/com/aws/greengrass/device/session/MqttSessionFactoryTest.java
+++ src/test/java/com/aws/greengrass/device/session/MqttSessionFactoryTest.java
@@ -6,8 +6,10 @@
 package com.aws.greengrass.device.session;
 
 import com.aws.greengrass.device.exception.AuthenticationException;
+import com.aws.greengrass.device.exception.CloudServiceInteractionException;
 import com.aws.greengrass.device.iot.IotAuthClient;
 import com.aws.greengrass.testcommons.testutilities.GGExtension;
+import org.hamcrest.core.IsNull;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -17,8 +19,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import software.amazon.awssdk.utils.ImmutableMap;
 
 import java.util.Map;
+import java.util.Optional;
 
-import org.hamcrest.core.IsNull;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
@@ -43,6 +45,7 @@ public class MqttSessionFactoryTest {
 
     @Test
     void GIVEN_credentialsWithUnknownClientId_WHEN_createSession_THEN_throwsAuthenticationException() {
+        when(mockIotAuthClient.getActiveCertificateId(any())).thenReturn(Optional.of("id"));
         when(mockIotAuthClient.isThingAttachedToCertificate(any(), any())).thenReturn(false);
 
         Assertions.assertThrows(AuthenticationException.class,
@@ -50,7 +53,24 @@ public class MqttSessionFactoryTest {
     }
 
     @Test
+    void GIVEN_credentialsWithInvalidCertificate_WHEN_createSession_THEN_throwsAuthenticationException() {
+        when(mockIotAuthClient.getActiveCertificateId(any())).thenReturn(Optional.empty());
+        Assertions.assertThrows(AuthenticationException.class,
+                () -> mqttSessionFactory.createSession(credentialMap));
+    }
+
+    @Test
+    void GIVEN_credentialsWithCertificate_WHEN_createSession_AND_cloudError_THEN_throwsAuthenticationException() {
+        when(mockIotAuthClient.getActiveCertificateId(any())).thenReturn(Optional.of("id"));
+        when(mockIotAuthClient.isThingAttachedToCertificate(any(), any()))
+                .thenThrow(CloudServiceInteractionException.class);
+        Assertions.assertThrows(AuthenticationException.class,
+                () -> mqttSessionFactory.createSession(credentialMap));
+    }
+
+    @Test
     void GIVEN_credentialsWithValidClientId_WHEN_createSession_THEN_returnsSession() throws AuthenticationException {
+        when(mockIotAuthClient.getActiveCertificateId(any())).thenReturn(Optional.of("id"));
         when(mockIotAuthClient.isThingAttachedToCertificate(any(), any())).thenReturn(true);
 
         Session session = mqttSessionFactory.createSession(credentialMap);
