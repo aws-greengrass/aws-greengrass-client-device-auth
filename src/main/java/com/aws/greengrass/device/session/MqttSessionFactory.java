1ecab44e9085da61d7364607b079ccf1d64625a9
saranya <nukai@amazon.com>
2022-05-20 00:30:54 -0700

    fix: check thing association with cert id during session creation

--- src/main/java/com/aws/greengrass/device/session/MqttSessionFactory.java
+++ src/main/java/com/aws/greengrass/device/session/MqttSessionFactory.java
@@ -6,11 +6,13 @@
 package com.aws.greengrass.device.session;
 
 import com.aws.greengrass.device.exception.AuthenticationException;
+import com.aws.greengrass.device.exception.CloudServiceInteractionException;
 import com.aws.greengrass.device.iot.Certificate;
 import com.aws.greengrass.device.iot.IotAuthClient;
 import com.aws.greengrass.device.iot.Thing;
 
 import java.util.Map;
+import java.util.Optional;
 import javax.inject.Inject;
 
 public class MqttSessionFactory implements SessionFactory {
@@ -31,14 +33,22 @@ public class MqttSessionFactory implements SessionFactory {
         //   we don't yet have the ability to authorize actions for those clients. So, for now, assume
         //   this is an IoT Thing and components will be specially handled elsewhere.
         Thing thing = new Thing(mqttCredential.clientId);
-        Certificate cert = new Certificate(mqttCredential.certificatePem);
-        if (!iotAuthClient.isThingAttachedToCertificate(thing, cert)) {
-            throw new AuthenticationException("unable to authenticate device");
+        Optional<String> certificateId;
+        try {
+            certificateId = iotAuthClient.getActiveCertificateId(mqttCredential.certificatePem);
+            if (!certificateId.isPresent()) {
+                throw new AuthenticationException("Certificate isn't active");
+            }
+            Certificate cert = new Certificate(certificateId.get());
+            if (!iotAuthClient.isThingAttachedToCertificate(thing, cert)) {
+                throw new AuthenticationException("unable to authenticate device");
+            }
+            Session session = new SessionImpl(cert);
+            session.putAttributeProvider(Thing.NAMESPACE, thing);
+            return session;
+        } catch (CloudServiceInteractionException e) {
+            throw new AuthenticationException("Failed to verify certificate with cloud", e);
         }
-
-        Session session = new SessionImpl(cert);
-        session.putAttributeProvider(Thing.NAMESPACE, thing);
-        return session;
     }
 
     private static class MqttCredential {
