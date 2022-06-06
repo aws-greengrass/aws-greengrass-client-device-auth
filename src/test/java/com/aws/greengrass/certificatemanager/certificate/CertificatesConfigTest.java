93c0b4e86d18293c991c726af34b256bcb737caf
Joe Butler <joebutl@amazon.com>
2022-06-06 14:03:51 -0600

    revert: configurable client cert rotation period

--- src/test/java/com/aws/greengrass/certificatemanager/certificate/CertificatesConfigTest.java
+++ src/test/java/com/aws/greengrass/certificatemanager/certificate/CertificatesConfigTest.java
@@ -64,18 +64,4 @@ public class CertificatesConfigTest {
                 is(equalTo(CertificatesConfig.DEFAULT_CLIENT_CERT_EXPIRY_SECONDS)));
     }
 
-    @Test
-    public void GIVEN_largeClientCertValidity_WHEN_getClientCertValiditySeconds_THEN_returnsMaxExpiry() {
-        configurationTopics.lookup(CertificatesConfig.PATH_CLIENT_CERT_EXPIRY_SECONDS)
-                .withValue(2 * CertificatesConfig.MAX_CLIENT_CERT_EXPIRY_SECONDS);
-        assertThat(certificatesConfig.getClientCertValiditySeconds(),
-                is(equalTo(CertificatesConfig.MAX_CLIENT_CERT_EXPIRY_SECONDS)));
-    }
-
-    @Test
-    public void GIVEN_smallClientCertValidity_WHEN_getClientCertValiditySeconds_THEN_returnsMinExpiry() {
-        configurationTopics.lookup(CertificatesConfig.PATH_CLIENT_CERT_EXPIRY_SECONDS).withValue(60 * 60 * 24); // 1 day
-        assertThat(certificatesConfig.getClientCertValiditySeconds(),
-                is(equalTo(CertificatesConfig.MIN_CLIENT_CERT_EXPIRY_SECONDS)));
-    }
 }
