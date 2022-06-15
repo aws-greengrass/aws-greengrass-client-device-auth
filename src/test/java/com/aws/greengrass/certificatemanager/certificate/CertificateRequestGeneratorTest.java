4a80cb32aa45a3426eada98583345803f4593b7e
Joe Butler <joebutl@amazon.com>
2022-06-15 15:21:25 -0600

    fix: properly construct certificate subject name

--- src/test/java/com/aws/greengrass/certificatemanager/certificate/CertificateRequestGeneratorTest.java
+++ src/test/java/com/aws/greengrass/certificatemanager/certificate/CertificateRequestGeneratorTest.java
@@ -63,23 +63,24 @@ class CertificateRequestGeneratorTest {
             "dy3+R8IfoAMuLPQmpFTr3Vfa/fmkeKv+K22NLVd5O3iOTPy+yMpWaG6pVTIkowxE\n" +
             "MQIDAQAB";
 
-    private static final String TEST_CERTIFICATE = "-----BEGIN CERTIFICATE REQUEST-----\n" +
-            "MIIC1jCCAb4CAQAwXDFaMFgGA1UEAxNRQ1NSR2VuZXJhdG9yVGVzdEM9VVNTVD1X\n" +
-            "YXNoaW5ndG9uTD1TZWF0dGxlTz1BbWF6b24uY29tIEluYy5PVT1BbWF6b24gV2Vi\n" +
-            "IFNlcnZpY2VzMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAsQh0983Q\n" +
-            "8Q0yB6hqZgMRYjOZj2ln+ZVmsAVME72fhNHFg3x2VjkJM87gtbtOWo4hnA9N4nTP\n" +
-            "0tqJSEJLikDKgYjX5DW5DQm7H8a50HJ+adsY+XFdU3DbBnplt5UShpFu0QlhjapZ\n" +
-            "Wh9HJlFSIXoj37DTKPaQMkNlp84ZFs3GOplnd+PWlp9JFsW39xKqME3Pqp48aUi5\n" +
-            "Go6Gcp3QJqLZcpdxSyf9ijW3xCrOCs9hEPeUswOiTRLj7wUNyUBl9jevr05Kk8Yj\n" +
-            "dDVGympxCUTbdy3+R8IfoAMuLPQmpFTr3Vfa/fmkeKv+K22NLVd5O3iOTPy+yMpW\n" +
-            "aG6pVTIkowxEMQIDAQABoDUwMwYJKoZIhvcNAQkOMSYwJDAiBgNVHREEGzAZhwTA\n" +
-            "qAEBghFzb21lZmFrZS5ob3N0LmNvbTANBgkqhkiG9w0BAQsFAAOCAQEABTSxHYom\n" +
-            "1RvxTDxvYCN06yXd8P5Egso4UUS9Mx9UQYRlCm5hI7wJuE+XkyhhRywbFyUMIBjT\n" +
-            "GchVT/0a1aqWvdBcj5ihElEHW5RbF/TcGVlVcecxCFrgkSNlaWbr7MT6Njl9Kjzv\n" +
-            "aKQ9rXJBzpYzeEDq3M27DI3JJo+Gm6W89QkUA17N4gDOJAamPp8bXgT2b9UEbiam\n" +
-            "J5Dg/2OPkJXx4GwjeWb1dlyiVAH8VM184X0Bgic53kobQdK2xriY+E/w3aFdPfhY\n" +
-            "8Qxp6HYHUH34ditVxY6tjGNh10CbG+5ELGN106mN1r67fQn+ptly2HI/jltQLVaI\n" +
-            "euAr+8WShnD05g==\n" +
+    private static final String TEST_CSR = "-----BEGIN CERTIFICATE REQUEST-----\n" +
+            "MIIDAjCCAeoCAQAwgYcxHDAaBgNVBAsTE0FtYXpvbiBXZWIgU2VydmljZXMxGDAW\n" +
+            "BgNVBAoTD0FtYXpvbi5jb20gSW5jLjEQMA4GA1UEBxMHU2VhdHRsZTETMBEGA1UE\n" +
+            "CBMKV2FzaGluZ3RvbjELMAkGA1UEBhMCVVMxGTAXBgNVBAMTEENTUkdlbmVyYXRv\n" +
+            "clRlc3QwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQCxCHT3zdDxDTIH\n" +
+            "qGpmAxFiM5mPaWf5lWawBUwTvZ+E0cWDfHZWOQkzzuC1u05ajiGcD03idM/S2olI\n" +
+            "QkuKQMqBiNfkNbkNCbsfxrnQcn5p2xj5cV1TcNsGemW3lRKGkW7RCWGNqllaH0cm\n" +
+            "UVIheiPfsNMo9pAyQ2WnzhkWzcY6mWd349aWn0kWxbf3EqowTc+qnjxpSLkajoZy\n" +
+            "ndAmotlyl3FLJ/2KNbfEKs4Kz2EQ95SzA6JNEuPvBQ3JQGX2N6+vTkqTxiN0NUbK\n" +
+            "anEJRNt3Lf5Hwh+gAy4s9CakVOvdV9r9+aR4q/4rbY0tV3k7eI5M/L7IylZobqlV\n" +
+            "MiSjDEQxAgMBAAGgNTAzBgkqhkiG9w0BCQ4xJjAkMCIGA1UdEQQbMBmHBMCoAQGC\n" +
+            "EXNvbWVmYWtlLmhvc3QuY29tMA0GCSqGSIb3DQEBCwUAA4IBAQBJZFtYJriJoh3c\n" +
+            "+oaAFsuoCgD48R1MXqC8ua0X9jSfa/HFu6453QHvZwFyNGJXmuGN3OtlRsUSx8QX\n" +
+            "dQBKzVuMxaQmT9gvOR1co6p4gUYx6xSyJ8kFVOuR7SlQ02VYR8ocSgjYNXKA3hYJ\n" +
+            "LbyPCBwGgyr1jt+xa98dXGC0jIuvMN8nDLGRUkMNnpPOM9S92bjpSBFGl6/+X5uo\n" +
+            "TkJHyuNqwXJzcDWAADhANSJx4d3OEyGs+JZOxjN+HcY+m73jHxugcqfzRX4oQ9ew\n" +
+            "84NxfASp1jLWU+0viwQUo4eH32U22WDZrgIbYsUWp5KHbwqpwTniE2OI1E5kp54i\n" +
+            "8lGRZDBC\n" +
             "-----END CERTIFICATE REQUEST-----\n";
 
     @Test
@@ -105,13 +106,10 @@ class CertificateRequestGeneratorTest {
         PrivateKey privateKey = kf.generatePrivate(new PKCS8EncodedKeySpec(encodedPrivateKey));
         KeyPair keyPair = new KeyPair(publicKey, privateKey);
         // Assert Cert Request generation
-        String expectedCSR = TEST_CERTIFICATE;
+        String expectedCSR = TEST_CSR;
         // Windows support
         expectedCSR = expectedCSR.replace("\n", System.lineSeparator());
         String actualCSR = CertificateRequestGenerator.createCSR(keyPair, thingName, ipAddresses, dnsNames);
         assertThat(actualCSR, is(expectedCSR));
-
     }
-
-    // TODO: Add additional test cases
 }
