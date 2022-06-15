df0cb6032da16cdea347aae001098656a34dc574
Vaibhav Murkute <murkutv@amazon.com>
2022-06-14 21:44:09 -0400

    feat: cache cloud request results

--- src/test/java/com/aws/greengrass/device/iot/IotAuthClientTest.java
+++ src/test/java/com/aws/greengrass/device/iot/IotAuthClientTest.java
@@ -8,6 +8,7 @@ package com.aws.greengrass.device.iot;
 import com.aws.greengrass.device.exception.CloudServiceInteractionException;
 import com.aws.greengrass.testcommons.testutilities.GGExtension;
 import com.aws.greengrass.util.GreengrassServiceClientFactory;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -35,9 +36,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.lenient;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith({MockitoExtension.class, GGExtension.class})
 public class IotAuthClientTest {
@@ -69,6 +68,11 @@ public class IotAuthClientTest {
         lenient().when(clientFactory.getGreengrassV2DataClient()).thenReturn(client);
     }
 
+    @AfterEach
+    void afterEach() {
+        iotAuthClient.clearLocalAuthCache();
+    }
+
     @Test
     void GIVEN_certificatePem_and_cloudProperResponse_WHEN_getActiveCertificateId_THEN_certificateIdReturned() {
         VerifyClientDeviceIdentityResponse response =
@@ -83,6 +87,23 @@ public class IotAuthClientTest {
     }
 
     @Test
+    void GIVEN_cached_certificatePem_WHEN_getActiveCertificateId_THEN_return_cached_certificateId() {
+        VerifyClientDeviceIdentityResponse response =
+                VerifyClientDeviceIdentityResponse.builder().clientDeviceCertificateId("certificateId").build();
+        when(client.verifyClientDeviceIdentity(any(VerifyClientDeviceIdentityRequest.class))).thenReturn(response);
+
+        Optional<String> certificateId = iotAuthClient.getActiveCertificateId("certificatePem");
+        assertThat(certificateId.get(), is("certificateId"));
+
+        // request certificateId for the same certificatePem multiple times;
+        // actual cloud request should be made only once and cached value should be returned for subsequent calls
+        assertThat(iotAuthClient.getActiveCertificateId("certificatePem").get(), is("certificateId"));
+        assertThat(iotAuthClient.getActiveCertificateId("certificatePem").get(), is("certificateId"));
+        verify(client, times(1))
+                .verifyClientDeviceIdentity(any(VerifyClientDeviceIdentityRequest.class));
+    }
+
+    @Test
     void GIVEN_cloudThrowValidationException_WHEN_getActiveCertificateId_THEN_returnNull(
             ExtensionContext context) {
         ignoreExceptionOfType(context, ValidationException.class);
@@ -90,6 +111,22 @@ public class IotAuthClientTest {
                 .thenThrow(ValidationException.class);
 
         assertThat(iotAuthClient.getActiveCertificateId("certificatePem"), is(Optional.empty()));
+    }
+
+    @Test
+    void GIVEN_cloudThrowValidationException_WHEN_getActiveCertificateId_THEN_should_not_cache_certificateId(ExtensionContext context) {
+        ignoreExceptionOfType(context, ValidationException.class);
+        when(client.verifyClientDeviceIdentity(any(VerifyClientDeviceIdentityRequest.class)))
+                .thenThrow(ValidationException.class);
+
+        assertThat(iotAuthClient.getActiveCertificateId("certificatePem"), is(Optional.empty()));
+
+        // request certificateId for the same invalid certificatePem multiple times;
+        // new cloud request should be made every time and result should not be cached
+        assertThat(iotAuthClient.getActiveCertificateId("certificatePem"), is(Optional.empty()));
+        assertThat(iotAuthClient.getActiveCertificateId("certificatePem"), is(Optional.empty()));
+        verify(client, times(3))
+                .verifyClientDeviceIdentity(any(VerifyClientDeviceIdentityRequest.class));
     }
 
     @Test
@@ -146,6 +183,24 @@ public class IotAuthClientTest {
     }
 
     @Test
+    void GIVEN_certificateAndThing_associated_locally_WHEN_isThingAttachedToCertificate_THEN_returnLocallyCachedResult() {
+        when(thing.getThingName()).thenReturn("thingName");
+        when(certificate.getIotCertificateId()).thenReturn("certificateId");
+        when(client.verifyClientDeviceIoTCertificateAssociation(
+                any(VerifyClientDeviceIoTCertificateAssociationRequest.class)))
+                .thenReturn(VerifyClientDeviceIoTCertificateAssociationResponse.builder().build());
+
+        assertThat(iotAuthClient.isThingAttachedToCertificate(thing, certificate), is(true));
+
+        // call isThingAttachedToCertificate for the same thingName and certificate multiple times;
+        // actual cloud request should be made only once and cached result should be returned for subsequent calls
+        assertThat(iotAuthClient.isThingAttachedToCertificate(thing, certificate), is(true));
+        assertThat(iotAuthClient.isThingAttachedToCertificate(thing, certificate), is(true));
+        verify(client, times(1))
+                .verifyClientDeviceIoTCertificateAssociation(any(VerifyClientDeviceIoTCertificateAssociationRequest.class));
+    }
+
+    @Test
     void GIVEN_cloudThrowValidationException_WHEN_isThingAttachedToCertificate_THEN_returnFalse(
             ExtensionContext context) {
         ignoreExceptionOfType(context, ValidationException.class);
@@ -155,6 +210,25 @@ public class IotAuthClientTest {
                 any(VerifyClientDeviceIoTCertificateAssociationRequest.class))).thenThrow(ValidationException.class);
 
         assertThat(iotAuthClient.isThingAttachedToCertificate(thing, certificate), is(false));
+    }
+
+    @Test
+    void GIVEN_cloudThrowValidationException_WHEN_isThingAttachedToCertificate_THEN_should_not_cache_results(
+            ExtensionContext context) {
+        ignoreExceptionOfType(context, ValidationException.class);
+        when(thing.getThingName()).thenReturn("thingName");
+        when(certificate.getIotCertificateId()).thenReturn("certificateId");
+        when(client.verifyClientDeviceIoTCertificateAssociation(
+                any(VerifyClientDeviceIoTCertificateAssociationRequest.class))).thenThrow(ValidationException.class);
+
+        assertThat(iotAuthClient.isThingAttachedToCertificate(thing, certificate), is(false));
+
+        // call isThingAttachedToCertificate for the same thingName and certificate multiple times;
+        // new cloud request should be made every time and result should not be cached
+        assertThat(iotAuthClient.isThingAttachedToCertificate(thing, certificate), is(false));
+        assertThat(iotAuthClient.isThingAttachedToCertificate(thing, certificate), is(false));
+        verify(client, times(3))
+                .verifyClientDeviceIoTCertificateAssociation(any(VerifyClientDeviceIoTCertificateAssociationRequest.class));
     }
 
     @Test
