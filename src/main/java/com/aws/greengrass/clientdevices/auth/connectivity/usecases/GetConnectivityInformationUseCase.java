7eaee216b8041626d6a52839bf87465f1198ac3e
Joe Butler <508579+jbutler@users.noreply.github.com>
2022-09-07 17:21:12 -0600

    feat: add GetConnectivityInformationUseCase

---
+++ src/main/java/com/aws/greengrass/clientdevices/auth/connectivity/usecases/GetConnectivityInformationUseCase.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.aws.greengrass.clientdevices.auth.connectivity.usecases;
+
+import com.aws.greengrass.clientdevices.auth.api.UseCases;
+import com.aws.greengrass.clientdevices.auth.connectivity.ConnectivityInformation;
+import com.aws.greengrass.clientdevices.auth.connectivity.HostAddress;
+
+import java.util.Set;
+import javax.inject.Inject;
+
+/**
+ * Get aggregated connectivity information.
+ */
+public class GetConnectivityInformationUseCase implements UseCases.UseCase<Set<HostAddress>, Void> {
+    private final ConnectivityInformation connectivityInformation;
+
+    @Inject
+    public GetConnectivityInformationUseCase(ConnectivityInformation connectivityInformation) {
+        this.connectivityInformation = connectivityInformation;
+    }
+
+    @Override
+    public Set<HostAddress> execute(Void dto) {
+        return connectivityInformation.getAggregatedConnectivityInformation();
+    }
+}
