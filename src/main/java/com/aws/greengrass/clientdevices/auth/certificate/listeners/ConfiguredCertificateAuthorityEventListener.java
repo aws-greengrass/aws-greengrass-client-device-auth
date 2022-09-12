a9ca314f238e240749a60b3b50ce570d133690a2
Joe Butler <508579+jbutler@users.noreply.github.com>
2022-09-12 17:00:16 -0600

    fix: implement DomainEventListener interface

--- src/main/java/com/aws/greengrass/clientdevices/auth/certificate/listeners/ConfiguredCertificateAuthorityEventListener.java
+++ src/main/java/com/aws/greengrass/clientdevices/auth/certificate/listeners/ConfiguredCertificateAuthorityEventListener.java
@@ -13,7 +13,8 @@ import com.aws.greengrass.clientdevices.auth.exception.UseCaseException;
 
 import javax.inject.Inject;
 
-public class ConfiguredCertificateAuthorityEventListener {
+public class ConfiguredCertificateAuthorityEventListener
+        implements DomainEvents.DomainEventListener<ConfiguredCertificateAuthorityEvent> {
     private final DomainEvents domainEvents;
 
 
@@ -30,9 +31,7 @@ public class ConfiguredCertificateAuthorityEventListener {
      * Listen for certificate authority change events.
      */
     public void listen() {
-        domainEvents.registerListener(
-                this::triggerCertificateAuthorityRegistrationUseCase,
-                ConfiguredCertificateAuthorityEvent.class);
+        domainEvents.registerListener(this, ConfiguredCertificateAuthorityEvent.class);
     }
 
     /**
@@ -40,7 +39,8 @@ public class ConfiguredCertificateAuthorityEventListener {
      *
      * @param event Certificate authority change event
      */
-    public void triggerCertificateAuthorityRegistrationUseCase(ConfiguredCertificateAuthorityEvent event) {
+    @Override
+    public void handle(ConfiguredCertificateAuthorityEvent event) {
         RegisterCertificateAuthorityUseCase useCase = UseCases.get(RegisterCertificateAuthorityUseCase.class);
         try {
             useCase.apply(null);
