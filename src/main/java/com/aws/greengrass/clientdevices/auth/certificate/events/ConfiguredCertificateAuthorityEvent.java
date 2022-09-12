797bbcb40f16237cc97ef9f6e2a30a5e32c6c9bf
Joe Butler <508579+jbutler@users.noreply.github.com>
2022-09-12 16:59:26 -0600

    feat: add DomainEvent interface

--- src/main/java/com/aws/greengrass/clientdevices/auth/certificate/events/ConfiguredCertificateAuthorityEvent.java
+++ src/main/java/com/aws/greengrass/clientdevices/auth/certificate/events/ConfiguredCertificateAuthorityEvent.java
@@ -5,15 +5,21 @@
 
 package com.aws.greengrass.clientdevices.auth.certificate.events;
 
+import com.aws.greengrass.clientdevices.auth.api.DomainEvent;
 import lombok.Getter;
 
 import java.security.cert.Certificate;
 
-public class ConfiguredCertificateAuthorityEvent {
+public class ConfiguredCertificateAuthorityEvent implements DomainEvent {
     @Getter
     private Certificate[] caCertificates;
 
     public ConfiguredCertificateAuthorityEvent(Certificate... caCertificates) {
         this.caCertificates = caCertificates;
+    }
+
+    @Override
+    public String getName() {
+        return this.getClass().getSimpleName();
     }
 }
