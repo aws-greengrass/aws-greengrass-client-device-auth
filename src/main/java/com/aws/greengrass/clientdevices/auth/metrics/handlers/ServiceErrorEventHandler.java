e6e5fd0339a85710a807909665d80e0ec9377806
Sameer Zuberi <samzub@amazon.com>
2022-12-12 08:09:29 -0800

    fix: add copyright header

--- src/main/java/com/aws/greengrass/clientdevices/auth/metrics/handlers/ServiceErrorEventHandler.java
+++ src/main/java/com/aws/greengrass/clientdevices/auth/metrics/handlers/ServiceErrorEventHandler.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package com.aws.greengrass.clientdevices.auth.metrics.handlers;
 
 import com.aws.greengrass.clientdevices.auth.api.DomainEvents;
