ddbba86d98e7a4ee4c5f1d060604a339b077f336
Eugene Smirnov <esmirnov@klika-tech.com>
2023-03-24 18:27:24 +0300

    fix(uat): added steps

--- uat/testing-features/src/main/java/com/aws/greengrass/steps/MqttControlSteps.java
+++ uat/testing-features/src/main/java/com/aws/greengrass/steps/MqttControlSteps.java
@@ -3,6 +3,7 @@ package com.aws.greengrass.steps;
 import io.cucumber.guice.ScenarioScoped;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 @ScenarioScoped
 public class MqttControlSteps {
@@ -22,14 +23,38 @@ public class MqttControlSteps {
         //@TODO Implement method
     }
 
-    @And("I connect {word} to {word}")
-    public void connectClientDeviceToTopic(String clientDeviceId, String topic) {
+    @And("I connect device {word} to broker")
+    public void connectClientDeviceToBroker(String clientDeviceId) {
         //@TODO Implement method
     }
 
-    @Then("device {string} is successfully connected to {string} within {int} {word}")
-    public void deviceIsSuccessfullyConnectedToWithinSeconds(String clientDeviceId, String topic, int value,
-                                                             String unit) {
+    @Then("device {string} is successfully connected to broker within {int} {word}")
+    public void deviceIsSuccessfullyConnectedToWithinSeconds(String clientDeviceId, int value, String unit) {
+        //@TODO Implement method
+    }
+
+    @When("I subscribe device {word} to {word} with qos {int}")
+    public void subscribeClientDeviceToTopic(String clientDeviceId, String topic, int qos) {
+        //@TODO Implement method
+    }
+
+    @Then("device {word} is successfully subscribed to {word}")
+    public void deviceIsSuccessfullySubscribedToTopic(String clientDeviceId, String topic) {
+        //@TODO Implement method
+    }
+
+    @When("I publish device {word} to {word} with qos {word} and message {string}")
+    public void publishClientDEviceToTopic(String clientDeviceId, String topic, int qos, String message) {
+        //@TODO Implement method
+    }
+
+    @Then("device {word} is successfully published message {string} to {word}")
+    public void deviceIsSuccessfullyPublishedMessage(String clientDeviceId, String message, String topic) {
+        //@TODO Implement method
+    }
+
+    @And("device {word} received from {word} message {string}")
+    public void deviceReceivedFromMessage(String clientDeviceId, String topic, String message) {
         //@TODO Implement method
     }
 }
--- uat/testing-features/src/main/resources/greengrass/features/ggmq-1.feature
+++ uat/testing-features/src/main/resources/greengrass/features/ggmq-1.feature
@@ -8,7 +8,7 @@ Feature: GGMQ-1
     And my device is running Greengrass
     And my MQTT Client Control is running
 
-  Scenario: GGMQ-1-T1: As a customer, I can connect client application to MQTT topic
+  Scenario: GGMQ-1-T1: As a customer, I can connect, subscribe and publish using client application to MQTT topic
     When I create a Greengrass deployment with components
       | aws.greengrass.clientdevices.Auth        | LATEST                                                        |
       | aws.greengrass.clientdevices.mqtt.EMQX   | LATEST                                                        |
@@ -16,9 +16,16 @@ Feature: GGMQ-1
       | aws.greengrass.client.Mqtt5JavaSdkClient | classpath:/greengrass/components/recipes/client_java_sdk.yaml |
     Then the Greengrass deployment is COMPLETED on the device after 300 seconds
     And I create client device "test" on "agent1" with the following policy
-      | operation | resource |
-      | connect   | "*"      |
+      | operation | resource     |
+      | connect   | "*"          |
+      | subscribe | "iot_data_0" |
+      | publish   | "iot_data_0" |
     And I associate "test" with ggc
-    And I connect "test" to "iot_data_0"
-    Then device "test" is successfully connected to "iot_data_0" within 3 seconds
+    And I connect device "test" to broker
+    Then device "test" is successfully connected to broker within 3 seconds
+    When I subscribe device "test" to "iot_data_0" with qos 0
+    Then device "test" is successfully subscribed to "iot_data_0"
+    When I publish device "test" to "iot_data_0" with qos 0 and message "Test message"
+    Then device "test" is successfully published message "Test message" to "iot_data_0"
+    And device "test" received from "iot_data_0" message "Test message"
 
