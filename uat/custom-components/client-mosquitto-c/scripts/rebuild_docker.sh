13d8b52f2bdefd5015e7860ad5554940d120b2bc
Vitaly Khalmansky <vkhalmansky@klika-tech.com>
2023-05-12 21:42:33 +0200

    feat(uat): integrate mosquitto client to project build and scenario run

--- uat/custom-components/client-mosquitto-c/scripts/rebuild_docker.sh
+++ uat/custom-components/client-mosquitto-c/scripts/rebuild_docker.sh
@@ -2,6 +2,6 @@
 
 rm -rf proto && cp -a ../../proto proto
 
-docker build -f Dockerfile --target builder -t client-mosquitto-c:builder .
+docker build -f Dockerfile --target builder -t client-mosquitto-c:builder-amd64 .
 
-# docker run -it --rm --name=client-mosquitto-c-builder client-mosquitto-c:builder bash
+# docker run -it --rm --name=client-mosquitto-c-builder client-mosquitto-c:builder-amd64 bash
