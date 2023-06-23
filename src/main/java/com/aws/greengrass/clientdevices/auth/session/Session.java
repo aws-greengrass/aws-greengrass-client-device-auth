572c73371e04eb4c23f874ebd7c6e8c929122b63
cyyan88 <cynthiayan88@gmail.com>
2023-06-23 14:06:56 -0700

    style: fix spacing

--- src/main/java/com/aws/greengrass/clientdevices/auth/session/Session.java
+++ src/main/java/com/aws/greengrass/clientdevices/auth/session/Session.java
@@ -1,47 +1,46 @@
- /*
+/*
  * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
 
- package com.aws.greengrass.clientdevices.auth.session;
+package com.aws.greengrass.clientdevices.auth.session;
 
- import com.aws.greengrass.clientdevices.auth.session.attribute.AttributeProvider;
- import com.aws.greengrass.clientdevices.auth.session.attribute.DeviceAttribute;
- 
- public interface Session {
- 
-     /**
-      * Get attribute provider by namespace.
-      *
-      * @param attributeProviderNameSpace Attribute namespace
-      * @return Attribute provider
-      */
-     AttributeProvider getAttributeProvider(String attributeProviderNameSpace);
- 
-     /**
-      * Get session attribute.
-      *
-      * @param attributeNamespace Attribute namespace
-      * @param attributeName      Attribute name
-      * @return Session attribute
-      */
-     DeviceAttribute getSessionAttribute(String attributeNamespace, String attributeName);
- 
-     /**
-      * Check if attribute provider exists.
-      *
-      * @param attributeProviderNameSpace Attribute namespace
-      * @return True/false
-      */
-     boolean containsAttributeProvider(String attributeProviderNameSpace);
- 
-     /**
-      * Check if attribute exists.
-      *
-      * @param attributeNamespace Attribute namespace
-      * @param attributeName      Attribute name
-      * @return True/false
-      */
-     boolean containsSessionAttribute(String attributeNamespace, String attributeName);
- }
- 
+import com.aws.greengrass.clientdevices.auth.session.attribute.AttributeProvider;
+import com.aws.greengrass.clientdevices.auth.session.attribute.DeviceAttribute;
+
+public interface Session {
+
+ /**
+  * Get attribute provider by namespace.
+  *
+  * @param attributeProviderNameSpace Attribute namespace
+  * @return Attribute provider
+  */
+ AttributeProvider getAttributeProvider(String attributeProviderNameSpace);
+
+ /**
+  * Get session attribute.
+  *
+  * @param attributeNamespace Attribute namespace
+  * @param attributeName      Attribute name
+  * @return Session attribute
+  */
+ DeviceAttribute getSessionAttribute(String attributeNamespace, String attributeName);
+
+ /**
+  * Check if attribute provider exists.
+  *
+  * @param attributeProviderNameSpace Attribute namespace
+  * @return True/false
+  */
+ boolean containsAttributeProvider(String attributeProviderNameSpace);
+
+ /**
+  * Check if attribute exists.
+  *
+  * @param attributeNamespace Attribute namespace
+  * @param attributeName      Attribute name
+  * @return True/false
+  */
+ boolean containsSessionAttribute(String attributeNamespace, String attributeName);
+}
--- src/main/java/com/aws/greengrass/clientdevices/auth/session/SessionImpl.java
+++ src/main/java/com/aws/greengrass/clientdevices/auth/session/SessionImpl.java
@@ -1,78 +1,77 @@
- /*
+/*
  * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
 
- package com.aws.greengrass.clientdevices.auth.session;
+package com.aws.greengrass.clientdevices.auth.session;
 
- import com.aws.greengrass.clientdevices.auth.session.attribute.AttributeProvider;
- import com.aws.greengrass.clientdevices.auth.session.attribute.DeviceAttribute;
- 
- import java.util.concurrent.ConcurrentHashMap;
- 
- public class SessionImpl extends ConcurrentHashMap<String, AttributeProvider> implements Session {
- 
-     static final long serialVersionUID = -1L;
- 
-     /**
-      * Create a Session from a list of attribute providers.
-      *
-      * @param providers list of attribute providers
-      */
-     public SessionImpl(AttributeProvider... providers) {
-         super();
-         for (AttributeProvider provider : providers) {
-             this.put(provider.getNamespace(), provider);
-         }
-     }
- 
-     @Override
-     public AttributeProvider getAttributeProvider(String attributeProviderNameSpace) {
-         return this.get(attributeProviderNameSpace);
-     }
- 
-     /**
-      * Get session attribute.
-      *
-      * @param attributeNamespace Attribute namespace
-      * @param attributeName      Attribute name
-      * @return Session attribute
-      */
-     @Override
-     public DeviceAttribute getSessionAttribute(String attributeNamespace, String attributeName) {
-         if (this.getAttributeProvider(attributeNamespace) != null) {
-             return this.getAttributeProvider(attributeNamespace).getDeviceAttributes().get(attributeName);
-         }
-         return null;
-     }
- 
+import com.aws.greengrass.clientdevices.auth.session.attribute.AttributeProvider;
+import com.aws.greengrass.clientdevices.auth.session.attribute.DeviceAttribute;
 
-     /**
-      * Check if attribute provider exists.
-      *
-      * @param attributeProviderNameSpace Attribute provider namespace
-      * @return True/false
-      */
-     @Override
-     public boolean containsAttributeProvider(String attributeProviderNameSpace) {
-         return this.containsKey(attributeProviderNameSpace);
-     }
- 
+import java.util.concurrent.ConcurrentHashMap;
 
-     /**
-      * Check if attribute exists.
-      *
-      * @param attributeNamespace Attribute namespace
-      * @param attributeName      Attribute name
-      * @return True/false
-      */
-     @Override
-     public boolean containsSessionAttribute(String attributeNamespace, String attributeName) {
-         return containsAttributeProvider(attributeNamespace)
-                     && this.getAttributeProvider(attributeNamespace)
-                     .getDeviceAttributes()
-                     .containsKey(attributeName);
+public class SessionImpl extends ConcurrentHashMap<String, AttributeProvider> implements Session {
+
+ static final long serialVersionUID = -1L;
+
+ /**
+  * Create a Session from a list of attribute providers.
+  *
+  * @param providers list of attribute providers
+  */
+ public SessionImpl(AttributeProvider... providers) {
+     super();
+     for (AttributeProvider provider : providers) {
+         this.put(provider.getNamespace(), provider);
      }
- 
  }
- 
+
+ @Override
+ public AttributeProvider getAttributeProvider(String attributeProviderNameSpace) {
+     return this.get(attributeProviderNameSpace);
+ }
+
+ /**
+  * Get session attribute.
+  *
+  * @param attributeNamespace Attribute namespace
+  * @param attributeName      Attribute name
+  * @return Session attribute
+  */
+ @Override
+ public DeviceAttribute getSessionAttribute(String attributeNamespace, String attributeName) {
+     if (this.getAttributeProvider(attributeNamespace) != null) {
+         return this.getAttributeProvider(attributeNamespace).getDeviceAttributes().get(attributeName);
+     }
+     return null;
+ }
+
+
+ /**
+  * Check if attribute provider exists.
+  *
+  * @param attributeProviderNameSpace Attribute provider namespace
+  * @return True/false
+  */
+ @Override
+ public boolean containsAttributeProvider(String attributeProviderNameSpace) {
+     return this.containsKey(attributeProviderNameSpace);
+ }
+
+
+ /**
+  * Check if attribute exists.
+  *
+  * @param attributeNamespace Attribute namespace
+  * @param attributeName      Attribute name
+  * @return True/false
+  */
+ @Override
+ public boolean containsSessionAttribute(String attributeNamespace, String attributeName) {
+     return containsAttributeProvider(attributeNamespace)
+                 && this.getAttributeProvider(attributeNamespace)
+                 .getDeviceAttributes()
+                 .containsKey(attributeName);
+ }
+
+}
