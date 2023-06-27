d9c22ee410a6db495446546fc81cff1cd74841cd
eschastlivtsev <58555129+eschastlivtsev@users.noreply.github.com>
2023-06-27 15:00:06 +0300

    feat(uat): implement delivery of received message (#345)

--- uat/custom-components/client-python-paho/src/grpc_client_server/grpc_discovery_client.py
+++ uat/custom-components/client-python-paho/src/grpc_client_server/grpc_discovery_client.py
@@ -9,7 +9,7 @@ import logging
 
 import grpc
 from exceptions.grpc_exception import GRPCException
-from grpc_client_server.grpc_generated.mqtt_client_control_pb2 import Mqtt5Disconnect, MqttConnectionId
+from grpc_client_server.grpc_generated.mqtt_client_control_pb2 import Mqtt5Disconnect, MqttConnectionId, Mqtt5Message
 from grpc_client_server.grpc_generated import mqtt_client_control_pb2_grpc, mqtt_client_control_pb2
 
 
@@ -87,12 +87,33 @@ class GRPCDiscoveryClient:
             self.__logger.exception(GRPC_REQUEST_FAILED)
             raise GRPCException(error) from error
 
-    def on_receive_mqtt_message(self):
+    def on_receive_mqtt_message(self, connection_id: int, mqtt_message: Mqtt5Message):
         """
         Called when MQTT message is receive my MQTT client
         and deliver information from it to gRPC server.
+        Parameters
+        ----------
+        connection_id - ID of connection, which received the message
+        mqtt_message - received message
         """
-        # TODO
+        mqtt_connection_id = MqttConnectionId(connectionId=connection_id)
+        request = mqtt_client_control_pb2.OnReceiveMessageRequest(
+            agentId=self.__agent_id, connectionId=mqtt_connection_id, msg=mqtt_message
+        )
+
+        self.__logger.info(
+            "Sending OnReceiveMessage request agent_id '%s' connection_id %i message %s on topic '%s'",
+            self.__agent_id,
+            connection_id,
+            mqtt_message.payload,
+            mqtt_message.topic,
+        )
+
+        try:
+            self.__stub.OnReceiveMessage(request)
+        except grpc.RpcError as error:
+            self.__logger.exception(GRPC_REQUEST_FAILED)
+            raise GRPCException(error) from error
 
     def on_mqtt_disconnect(self, connection_id: int, disconnect_info: Mqtt5Disconnect, error_string: str):
         """
