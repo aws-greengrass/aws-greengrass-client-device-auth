4d8fe35c7bef2f9c1b491d8abde5a1dde2002118
Egor Schastlivtsev <eschastlivtsev@klika-tech.com>
2023-08-03 17:03:03 +0300

    feat(uat): add errors handling for paho python client build on windows

--- uat/custom-components/client-python-paho/scripts/build.bat
+++ uat/custom-components/client-python-paho/scripts/build.bat
@@ -6,15 +6,20 @@
 @echo off
 set start_dir=%cd%
 cd %~dp0../src
-pip install virtualenv
-python -m venv dev-env
-call dev-env\Scripts\activate.bat
-pip install pyinstaller
-pip install -r requirements.txt
-python -m grpc_tools.protoc -I../../../proto --python_out=../src/grpc_client_server/grpc_generated --pyi_out=../src/grpc_client_server/grpc_generated --grpc_python_out=../src/grpc_client_server/grpc_generated ../../../proto/mqtt_client_control.proto
-python fix_generated.py
-pyinstaller client-python-paho.spec
-move dist\client-python-paho.exe ..\
-rmdir /s /q dist
-rmdir /s /q build
-cd %start_dir%
+pip install virtualenv || (cd %start_dir% & exit /b 1)
+python -m venv dev-env || (cd %start_dir% & exit /b 2)
+call dev-env\Scripts\activate.bat || (cd %start_dir% & exit /b 3)
+pip install pyinstaller || (cd %start_dir% & deactivate & exit /b 4)
+pip install -r requirements.txt || (cd %start_dir% & deactivate & exit /b 5)
+
+python -m grpc_tools.protoc -I../../../proto --python_out=../src/grpc_client_server/grpc_generated ^
+--pyi_out=../src/grpc_client_server/grpc_generated --grpc_python_out=../src/grpc_client_server/grpc_generated ^
+../../../proto/mqtt_client_control.proto || (cd %start_dir% & deactivate & exit /b 6)
+
+python fix_generated.py || (cd %start_dir% & deactivate & exit /b 7)
+pyinstaller client-python-paho.spec || (cd %start_dir% & deactivate & rmdir /s /q dist & rmdir /s /q build & exit /b 8)
+move dist\client-python-paho.exe ..\ || (cd %start_dir% & deactivate & rmdir /s /q dist & rmdir /s /q build & exit /b 9)
+rmdir /s /q dist || (cd %start_dir% & deactivate & rmdir /s /q build & exit /b 10)
+rmdir /s /q build || (cd %start_dir% & deactivate & exit /b 11)
+cd %start_dir% || (deactivate & exit /b 12)
+deactivate || exit /b 13
