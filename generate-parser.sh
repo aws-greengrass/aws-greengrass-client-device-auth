631bab58d89bddee5158f9e50a84cf128b39983c
Joseph Cosentino <joecose@amazon.com>
2023-11-27 13:23:51 -0800

    chore: script to generate parser (#413)

---
+++ generate-parser.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+set -e
+jjtree -OUTPUT_DIRECTORY=src/main/java/com/aws/greengrass/clientdevices/auth/configuration/parser src/main/javacc/RuleExpression.jjt
+javacc -OUTPUT_DIRECTORY=src/main/java/com/aws/greengrass/clientdevices/auth/configuration/parser src/main/java/com/aws/greengrass/clientdevices/auth/configuration/parser/RuleExpression.jj
+mvn license:format
